b8bce1b3ee7c32769bd8871c0f1196f55cbc193a
Tobias Hargesheimer <tobias@ison.ws>
2022-11-27 19:18:22 +0100

    delete file

--- mount_smb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# EXAMPLE, change the settings !
-
-# Required package (on Debian/Ubuntu), install: sudo apt-get install cifs-utils
-
-sudo mount -t cifs -o uid=1000,gid=1000,file_mode=0775,dir_mode=0775,username=USERNAME,password=PASSWORD //IP/and/path ${PWD}/Music/smbmusic
